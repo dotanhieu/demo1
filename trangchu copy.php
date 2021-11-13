f1f46adadbe898730f78539bb49f9785bd26b6b0
dotanhieu <dotanhieu1@gmail.com>
2021-11-13 10:31:28 +0700

    nhu hach

---
+++ trangchu copy.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	
+<meta charset="utf-8">
+<link href="css/sty.css" rel="stylesheet" type="text/css" media="all" />
+<style type="text/css">
+body {
+    background-image: url(images/New%20folder/4.jpeg);
+}
+body,td,th {
+    font-family: Constantia, "Lucida Bright", "DejaVu Serif", Georgia, serif;
+}
+</style>
+</head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+<body>
+<div class="container">
+	
+	
+		<p> Trang Quản Lý </p>
+		
+	<div id=menu>
+				<ul>
+					<li><a href="admin/qlsp/index.php">Quản lý sản phẩm</a></li>
+					<li><a href="admin/index.php">Quản lý user</a></li>
+				</ul>
+  </div>
+</div>
+</body>
+</html>
